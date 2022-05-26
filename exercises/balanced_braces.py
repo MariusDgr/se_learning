fecfaa7840f3978a300480abf0ceb559cd6c69fd
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2022-05-26 19:44:41 +0300

    prind bracket recursive

---
+++ exercises/balanced_braces.py
@@ -0,0 +1,24 @@
+def print_brackets(n):
+    stack = []
+    res = []
+
+    def backtrack(openN, closedN):
+        if openN == closedN == n:
+            res.append("".join(stack))
+            return
+    
+        if openN < n:
+            stack.append("(")
+            backtrack(openN+1, closedN)
+            stack.pop()
+
+        if closedN < openN:
+            stack.append(")")
+            backtrack(openN, closedN + 1)
+            stack.pop()
+
+    backtrack(0, 0)
+    return res
+    
+if __name__ == "__main__":
+    print(print_brackets(3))
