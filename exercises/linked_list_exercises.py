6e8571c5ef405f7c14ea79698ebb4bedb521c2f8
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2022-06-04 10:24:59 +0300

    linked list remove val

--- exercises/linked_list_exercises.py
+++ exercises/linked_list_exercises.py
@@ -1,3 +1,38 @@
+def hasCycle(self, head) -> bool:
+    if head is None:
+            return False
+        
+    node = head
+    nodes = set([node])
+    
+    while node.next is not None:
+        node = node.next
+        if node in nodes:
+            return True
+        else:
+            nodes.add(node)
+        
+    return False
+
+def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+    new_head = None
+    prev = None
+    cur = head
+    
+    while cur:
+        if cur.val == val:
+            if prev:
+                prev.next = cur.next
+                
+        else:
+            if not new_head:
+                new_head = cur
+
+            prev = cur
+        cur = cur.next
+            
+    return new_head
+
 """
 # Definition for a Node.
 class Node:
