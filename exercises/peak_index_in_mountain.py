bb4aa258eed6c2d0b8aa2fdd9ec2105ab8861b3c
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2022-06-07 10:24:23 +0300

    peak finder binary

--- exercises/peak_index_in_mountain.py
+++ exercises/peak_index_in_mountain.py
@@ -10,13 +10,20 @@ def peakIndexInMountainArray(self, arr: list[int]) -> int:
             
 def peakIndexInMountainArrayBnSrch(arr):
     n = len(arr)
+        
+    if n == 0:
+        return None
+    
+    if n == 1:
+        return 0
+    
     l = 0
     r = n - 1
-    while l < r:
+    while l <= r:
         m = (l + r) // 2
-        if m - 1 > 0 and arr[m-1] > arr[m]:
-            r = m
-        elif m + 1 < n - 1 and arr[m+1] > arr[m]:
-            l = m
+        if m - 1 >= 0 and arr[m-1] > arr[m]:
+            r = m - 1
+        elif m + 1 <= n - 1 and arr[m+1] > arr[m]:
+            l = m + 1
         else:
             return m
