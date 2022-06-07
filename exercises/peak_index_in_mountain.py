222168aebbc714d151071d02f44e77ce2d9f4905
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2022-06-07 09:55:26 +0300

    peak finding v1

---
+++ exercises/peak_index_in_mountain.py
@@ -0,0 +1,10 @@
+def peakIndexInMountainArray(self, arr: list[int]) -> int:
+    max_ind = 0
+    max_val = -1
+    for i, val in enumerate(arr):
+        if max_val < val:
+            max_ind = i
+            max_val = val
+        
+            
+    return max_ind
