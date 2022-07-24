89939f4325f62d34112c8516a1d6de1e5a24e41b
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2022-07-24 23:30:14 +0300

    duplicate with constant extra space

---
+++ exercises/duplicate_in_array.py
@@ -0,0 +1,12 @@
+"Leetcode 442"
+def findDuplicates(self, nums: List[int]) -> List[int]:
+    res = []
+    
+    for n in nums:
+        index = abs(n) -1
+        if nums[index] < 0:
+            res.append(abs(n))
+        
+        nums[index] = -1 * nums[index]
+
+    return res
