8c3d2159a1380851bae843eae9f9ae33965485e5
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2022-07-24 20:55:10 +0300

    floyd's algorithm for duplicates

--- exercises/contains_duplicate.py
+++ exercises/contains_duplicate.py
@@ -6,6 +6,23 @@ def containsDuplicate(nums: list[int]) -> bool:
         found.add(val)
     return False
 
+def findDuplicate(self, nums: List[int]) -> int:
+    """Floyd's algorithm"""
+    slow, fast = 0, 0
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        
+        if slow == fast:
+            break
+            
+    slow2 = 0
+    while True:
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if slow == slow2:
+            return slow
+
 if __name__ == "__main__":
     print(containsDuplicate([1, 2, 3, 4, 5]))
     print(containsDuplicate([1, 4, 4, 5]))
