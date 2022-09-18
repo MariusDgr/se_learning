a91a97655e796462b4f8dc7ce76b89f12a93141b
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2022-09-18 21:53:33 +0300

    subsets with duplicates

--- exercises/subsets.py
+++ exercises/subsets.py
@@ -6,3 +6,16 @@ def subsets(self, nums: List[int]) -> List[List[int]]:
             dfs(j + 1, path + [nums[j]])
     dfs(0, [])
     return res
+
+
+def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+    nums.sort()
+    res = []
+    def dfs(i, path):
+        res.append(path)
+        for j in range(i, len(nums)):
+            if j > i and nums[j] == nums[j - 1]:
+                continue
+            dfs(j + 1, path + [nums[j]])
+    dfs(0, [])
+    return res
