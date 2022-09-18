f65edf2011d4873dc4eea90a576285d88f8885db
MariusDgr <32437161+MariusDgr@users.noreply.github.com>
2022-09-18 20:31:45 +0300

    more exercises

---
+++ exercises/letter_permutation.py
@@ -0,0 +1,16 @@
+def letterCasePermutation(self, s: str) -> List[str]:
+    
+    res = []
+    def dfs(i, path):
+        if i == len(s):
+            res.append(path)
+            return
+
+        if s[i].isalpha():
+            dfs(i + 1, path + s[i].lower())
+            dfs(i + 1, path + s[i].upper())
+        else:
+            dfs(i + 1, path + s[i])
+
+    dfs(0, "")
+    return res
